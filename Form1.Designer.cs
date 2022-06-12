a78f87c01d53c6866eae3deb6251723e36ca0ffa
n*! <94866436+naz-bdeb@users.noreply.github.com>
2022-06-11 23:04:44 -0400

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace kagiForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EditionText = new System.Windows.Forms.Label();
+            this.ActivationHeader = new System.Windows.Forms.Label();
+            this.ActivationText = new System.Windows.Forms.Label();
+            this.kagiVersionLabel = new System.Windows.Forms.Label();
+            this.activateButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.deactivateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(357, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(675, 428);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(113, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "View the Source Code";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.contributeLink);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(349, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Windows Edition";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EditionText
+            // 
+            this.EditionText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EditionText.Location = new System.Drawing.Point(354, 217);
+            this.EditionText.Name = "EditionText";
+            this.EditionText.Size = new System.Drawing.Size(107, 66);
+            this.EditionText.TabIndex = 3;
+            this.EditionText.Text = "Placeholder";
+            this.EditionText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ActivationHeader
+            // 
+            this.ActivationHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ActivationHeader.AutoSize = true;
+            this.ActivationHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActivationHeader.Location = new System.Drawing.Point(351, 295);
+            this.ActivationHeader.Name = "ActivationHeader";
+            this.ActivationHeader.Size = new System.Drawing.Size(117, 18);
+            this.ActivationHeader.TabIndex = 4;
+            this.ActivationHeader.Text = "Activation Status";
+            // 
+            // ActivationText
+            // 
+            this.ActivationText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ActivationText.Location = new System.Drawing.Point(356, 324);
+            this.ActivationText.Name = "ActivationText";
+            this.ActivationText.Size = new System.Drawing.Size(105, 61);
+            this.ActivationText.TabIndex = 5;
+            this.ActivationText.Text = "Placeholder";
+            this.ActivationText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // kagiVersionLabel
+            // 
+            this.kagiVersionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.kagiVersionLabel.Location = new System.Drawing.Point(354, 132);
+            this.kagiVersionLabel.Name = "kagiVersionLabel";
+            this.kagiVersionLabel.Size = new System.Drawing.Size(107, 32);
+            this.kagiVersionLabel.TabIndex = 6;
+            this.kagiVersionLabel.Text = "kagiVersionLabel";
+            this.kagiVersionLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // activateButton
+            // 
+            this.activateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.activateButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.activateButton.Location = new System.Drawing.Point(352, 401);
+            this.activateButton.Name = "activateButton";
+            this.activateButton.Size = new System.Drawing.Size(116, 23);
+            this.activateButton.TabIndex = 7;
+            this.activateButton.Text = "Activate !";
+            this.activateButton.UseVisualStyleBackColor = true;
+            this.activateButton.Click += new System.EventHandler(this.activate_click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 418);
+            this.progressBar.Maximum = 1;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 8;
+            // 
+            // deactivateButton
+            // 
+            this.deactivateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.deactivateButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.deactivateButton.Location = new System.Drawing.Point(352, 372);
+            this.deactivateButton.Name = "deactivateButton";
+            this.deactivateButton.Size = new System.Drawing.Size(116, 23);
+            this.deactivateButton.TabIndex = 9;
+            this.deactivateButton.Text = "Deactivate !";
+            this.deactivateButton.UseVisualStyleBackColor = true;
+            this.deactivateButton.Click += new System.EventHandler(this.deactivateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deactivateButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.activateButton);
+            this.Controls.Add(this.kagiVersionLabel);
+            this.Controls.Add(this.ActivationText);
+            this.Controls.Add(this.ActivationHeader);
+            this.Controls.Add(this.EditionText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kagi 0.1a";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label EditionText;
+        private System.Windows.Forms.Label ActivationHeader;
+        private System.Windows.Forms.Label ActivationText;
+        private System.Windows.Forms.Label kagiVersionLabel;
+        private System.Windows.Forms.Button activateButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button deactivateButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace kagiForms
+
+{
+    using SLID = Guid;
+    public partial class Form1 : Form
+    {
+        String edition;
+        public Form1()
+        {
+            InitializeComponent();
+            isConnectedToInternet();
+
+            kagiVersionLabel.Text = "Kagi 0.1a";
+
+            EditionText.Text = getEdition().ToString();
+            edition = getEdition().ToString();
+
+            bool activated = IsGenuineWindows();
+            if (activated)
+            {
+                ActivationText.Text = "Your OS is already activated";
+                ActivationText.ForeColor = System.Drawing.Color.Green;
+                activateButton.Enabled = false;
+            }
+            else
+            {
+                ActivationText.Text = "Your OS is not currently activated";
+                ActivationText.ForeColor = System.Drawing.Color.Red;
+                deactivateButton.Enabled = false;
+            }
+        }
+
+        private void isConnectedToInternet()
+        {
+            try
+            {
+                System.Net.NetworkInformation.Ping pingSender = new System.Net.NetworkInformation.Ping();
+                System.Net.NetworkInformation.PingReply reply = pingSender.Send("google.com");
+            }
+            catch
+            {
+                MessageBox.Show("Kagi couldn't verify you were connected to the Internet. " +
+                        "Please connect your device to the Internet before relaunching Kagi.",
+                        "No Internet !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+        }
+
+        
+        private string getEdition()
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion");
+            return registryKey.GetValue("ProductName").ToString();
+        }
+
+        public enum SL_GENUINE_STATE
+        {
+            SL_GEN_STATE_IS_GENUINE = 0,
+            SL_GEN_STATE_INVALID_LICENSE = 1,
+            SL_GEN_STATE_TAMPERED = 2,
+            SL_GEN_STATE_OFFLINE = 3,
+            SL_GEN_STATE_LAST = 4
+        }
+        
+        public enum ERROR_CODES : int
+        {
+            // IPK (When changing the key)
+            NO_ADMIN = -1073418203,
+            INVALID_KEY = -1073418160,
+            // ATO (When reloading license status)
+            LICENSE_MANAGEMENT_ERROR = -1073418124,
+            SUCCESS = 0
+        }
+
+        [DllImportAttribute("Slwga.dll", EntryPoint = "SLIsGenuineLocal", CharSet = CharSet.None, ExactSpelling = false, SetLastError = false, PreserveSig = true, CallingConvention = CallingConvention.Winapi, BestFitMapping = false, ThrowOnUnmappableChar = false)]
+        [PreserveSigAttribute()]
+        internal static extern uint SLIsGenuineLocal(ref SLID slid, [In, Out] ref SL_GENUINE_STATE genuineState, IntPtr val3);
+
+
+        public static bool IsGenuineWindows()
+        {
+            bool _IsGenuineWindows = false;
+            Guid ApplicationID = new Guid("55c92734-d682-4d71-983e-d6ec3f16059f"); //Application ID GUID http://technet.microsoft.com/en-us/library/dd772270.aspx
+            SLID windowsSlid = (Guid)ApplicationID;
+            try
+            {
+                SL_GENUINE_STATE genuineState = SL_GENUINE_STATE.SL_GEN_STATE_LAST;
+                uint ResultInt = SLIsGenuineLocal(ref windowsSlid, ref genuineState, IntPtr.Zero);
+                if (ResultInt == 0)
+                {
+                    _IsGenuineWindows = (genuineState == SL_GENUINE_STATE.SL_GEN_STATE_IS_GENUINE);
+                }
+                else
+                {
+                    Console.WriteLine("Error getting information {0}", ResultInt.ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return _IsGenuineWindows;
+        }
+
+        private String readKeyFromFile()
+        {
+            try { String text = System.IO.File.ReadAllText(".\\key.txt"); return text; }
+            catch { return null; }
+        }
+
+        private bool isKeyValid(String key)
+        {
+            const int CORRECT_KEY_LENGTH = 5;
+
+            if (key == null) { 
+                MessageBox.Show("Kagi couldn't find the key.txt file, " +
+                    "make sure it is placed in the same directory as the executable", "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                return false; 
+            }
+
+            String[] keysArr = key.Split('-');
+
+            if (keysArr.Length != CORRECT_KEY_LENGTH) {
+                MessageBox.Show("The key is not the correct length, " +
+                    "make sure the total length of the key is 25.", "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false; 
+            }
+
+            foreach (String keyPart in keysArr)
+            {
+                if (keyPart.Length != CORRECT_KEY_LENGTH) {
+                    MessageBox.Show("The 5 parts of the key are invalid. " +
+                        "Make sure each part has 5 alphanumeral characters. ", 
+                        "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false; 
+                }
+            }
+
+            MessageBox.Show("Kagi will now try to activate " + edition +
+                        " with the key " + key,
+                        "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return true;
+        }
+
+        private void activate_click(object sender, EventArgs e)
+        {
+            String key = readKeyFromFile();
+
+            if (!isKeyValid(key))
+            {
+                return;
+            }
+
+            switch (edition)
+            {
+                case "Windows 10 Pro":
+                    // code to install pro keys
+                    activateOS(key);
+                    break;
+
+                case "Windows 10 Home":
+                    // code to install home keys
+                    activateOS(key);
+                    break;
+
+                default:
+                    MessageBox.Show("Your current OS edition is not currently supported !" +
+                "\n\nThe current editions supported by Kagi are Windows Home and Professionnal.",
+                "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+        private bool isError(int code)
+        {
+            switch (code)
+            {
+                case (int)ERROR_CODES.NO_ADMIN:
+                    MessageBox.Show("The program is missing admin privileges. Please make sure you run Kagi as admin.",
+                    "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return true;
+
+                case (int)ERROR_CODES.INVALID_KEY:
+                    MessageBox.Show("There was a problem installing the product key on your system !" +
+                    "Please ensure that the product key you entered was made for your Windows Edition and try again.",
+                    "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return true;
+
+                case (int)ERROR_CODES.LICENSE_MANAGEMENT_ERROR:
+                    MessageBox.Show("There was a problem installing the product key on your system !" +
+                    "This is usually caused by an invalid communication with the activation server. Please try again later.",
+                    "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return true;
+
+                case 0:
+                    return false;
+
+                default:
+                    MessageBox.Show("An unknown error occured. Please try again later.",
+                    "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return true;
+
+            }
+        }
+
+        private void activateOS(String key)
+        {
+            List<String> commands = new List<String>();
+
+            commands.Add("slmgr.vbs /ipk " + key);
+            commands.Add("slmgr.vbs /skms kms.lotro.cc");
+            commands.Add("slmgr.vbs /ato");
+
+            int exitCode;
+
+            foreach (String command in commands)
+            {
+                exitCode = executeCommand(command);
+                if (isError(exitCode)) { return; }
+            }
+            progressBar.PerformStep();
+
+            MessageBox.Show("Your OS was successfully activated !",
+                    "Success !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ActivationText.Text = "Your OS is now activated";
+            ActivationText.ForeColor = System.Drawing.Color.Green;
+            activateButton.Enabled = false;
+            deactivateButton.Enabled = true;
+
+
+        }
+
+        private int executeCommand(String command)
+        {
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+            // get exit code of the process
+            int code = process.ExitCode;
+            return code;
+        }
+
+
+        private void contributeLink(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/naz-bdeb/kagiActivator",
+                UseShellExecute = true
+            });
+        }
+
+        private void deactivateButton_Click(object sender, EventArgs e)
+        {
+            executeCommand("slmgr.vbs -upk");
+            MessageBox.Show("Your OS was successfully deactivated !",
+                    "Success !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            deactivateButton.Enabled = false;
+            activateButton.Enabled = true;
+        }
+    }
+}
